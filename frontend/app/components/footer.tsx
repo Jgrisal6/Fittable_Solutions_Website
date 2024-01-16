a024b31ade77efea19c5fa5ebcf871a58f83f411
Juan Pablo Grisales <esteban@JPablos-MacBook-Pro.local>
2024-01-15 20:08:09 -0500

    Adding structure to components. change carousel to banner

---
+++ frontend/app/components/footer.tsx
@@ -0,0 +1,18 @@
+'use client'
+ export default function Footer() {
+    return (
+        <footer>
+            <div>
+                <img src="" alt="" />
+                <ul>
+                    <li>
+                        <p>About us</p>
+                    </li>
+                    <li>
+                        <p> Contact us</p>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+ }
---
+++ frontend/app/components/navbar.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from "react";
+
+export default function Navbar () {
+    <nav>
+        <div>
+            <img src="" alt="" />
+            <ul>
+                <li>Projects</li>
+                <li>About us</li>
+            </ul>
+        </div>
+    </nav>
+}
