4032fbee75c3c2cd2ed77db9c2442b822b5809f2
jgrisal7 <grisalesjuanpablo18@gmail.com>
2024-01-19 17:53:40 -0500

    Adding material-tailwind/react and, setting tool. Second attempt

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,17 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
 
 const config: Config = {
   content: [
