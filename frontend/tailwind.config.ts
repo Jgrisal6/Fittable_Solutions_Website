fd3fa28978dada6701d8d6ec6a3bbb0abe48c2f0
jgrisal7 <grisalesjuanpablo18@gmail.com>
2024-01-19 18:30:52 -0500

    Fixing material-tailwind settings

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,23 +1,11 @@
 import type { Config } from 'tailwindcss';
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
+const config: Config = ({
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx, mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -29,5 +17,6 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+});
+
+module.exports = withMT(config);
