86d7055d17c7f2293ce63c0ef6f68c6aa1d2056d
Juan Pablo Grisales <esteban@JPablos-MacBook-Pro.local>
2024-01-21 11:18:28 -0500

    Setting material-tailwind configuration in tailwind.config.ts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
+import Image from 'next/image';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 export default function Home() {
   return (
