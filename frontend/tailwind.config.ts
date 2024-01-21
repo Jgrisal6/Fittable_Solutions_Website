8d5ba2e9ac288ea08741ddb42288bb6cf44f766f
Juan Pablo Grisales <esteban@JPablos-MacBook-Pro.local>
2024-01-21 12:35:24 -0500

    Styling NavBar component

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,6 +9,11 @@ const config: Config = ({
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'blue': '#1e3a8a',
+      'blue-button': '#172554',
+      'gray': '#d1d5db',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
